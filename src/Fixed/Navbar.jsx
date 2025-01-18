4401e200bf630b8dc78fa663736f136d6b2cb499
Yetty2020 <fatihaadetoro@gmail.com>
2025-01-18 08:32:42 -0800

    chages

--- src/Fixed/Navbar.jsx
+++ src/Fixed/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Logo from '../assets/logo.png'
+import Logo from './assets/logo.png'
 
 import { RiMenu3Fill } from "react-icons/ri";
 import { RiCloseFill } from "react-icons/ri";
